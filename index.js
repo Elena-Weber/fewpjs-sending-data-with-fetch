c3cc1d6a15db8392c773276e0b0f6283d229db39
Elena-Weber <crochetmiracles@gmail.com>
2021-06-01 21:59:04 -0400

    commit tree f7a80881e277fb842d60ef923f71bd99b4271f89

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your code here
+let formData = { name: "Steve", email: "steve@steve.com"};
+function submitData() {
+let obj = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+};
+    return fetch("http://localhost:3000/users", obj)
+    .then(function(response) {
+        return response.json() })
+        .then(function(object) {
+            document.body.innerHTML = object[ "id" ] })
+            .catch(function(error) {
+                document.body.innerHTML = error.message })
+}
